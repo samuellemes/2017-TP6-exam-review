b37372930408ca3c1f1608d28141f755800b29f1
Samuel Lemes <samuellemes25@gmail.com>
2017-11-22 20:23:57 -0200

    Update check-students.js

--- app/module/check-students.js
+++ app/module/check-students.js
@@ -1,4 +1,52 @@
 const students = require('../../db/studentsDB')
 
-console.log(students[0])
+function getStudentId() {
+    return process.argv[2] || null
+}
 
+function byId(student) {
+    return student.id == getStudentId()
+}
+
+function getAverage(grades) {
+    return grades.reduce((a, b) => a + b) / grades.length
+}
+
+function getStatus(average) {
+    let status = 'Approved'
+    
+    if(average < 50) {
+        status = 'Not Approved'
+    }
+    else if(average < 60) {
+        status = 'Final'
+    }
+
+    return status
+}
+
+const student = students.find(byId)
+
+// if (student) {
+//     const average = getAverage(student.grades)
+//     const status = getStatus(average)
+
+//     console.log('Name: ' + student.name)
+//     console.log('Average: ' + average)
+//     console.log('Status: ' + status)
+// }
+// else {
+//     console.log('Student not found!')
+// }
+
+module.exports = {
+    getStudentStatus(id) {
+        let student = students.find(student => student.id == id)
+        if(student) {
+            const average = getAverage(student.grades)
+            student.average = average
+            student.status = getStatus(average)
+        }
+        return student
+    }
+}
