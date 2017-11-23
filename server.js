5260b73243cd32c6e1bc814e20093abff794af09
Samuel Lemes <samuellemes25@gmail.com>
2017-11-23 01:01:27 -0200

    Update index ejs and server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const app = express()
 const path = require('path')
@@ -6,14 +5,27 @@ const PORT = 3000
 
 // setup express
 const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
 // template
-
+app.set('views', path.join(__dirname, 'public'))
+app.set('view engine', 'ejs')
 
 app.get('/', (request, response) => {
-    response.render('index')
+    const data = { student: null, submitted: false }
+    response.render('index', data)
+})
+
+app.get('/status', (request, response) => {
+    const { getStudentStatus } = require('./app/check-students')
+    studentStatus = getStudentStatus(request.query.studentId)
+
+    const data = {
+        student: studentStatus,
+        submitted: true
+    }
+    response.render('index', data)
 })
 
 app.listen(PORT, () => {
